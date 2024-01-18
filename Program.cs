fc213d693a1380396ef669d35a0968d397fcdece
zekegoodman <zekegoodman@ZEKEGOODMANB9EC>
2024-01-17 23:12:47 -0700

    add comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Mission2Real;
+using System.Runtime.InteropServices;
 
 
 internal class Program
@@ -8,22 +9,28 @@ internal class Program
         Console.WriteLine("Welcome to the dice throwing simulator!");
         Console.WriteLine("How many dice rolls would you like to simulate? (Please enter a number)");
 
+        // collect dice roll count from user and convert to integer
         string rollCount = "";
         rollCount = Console.ReadLine();
         int iRollCount = int.Parse(rollCount);
 
+        // create new instance of the rollDice class from mission2Real namespace, create list and pass the number of rolls
         rollDice rollDice = new rollDice();
         int[] rollTotals = rollDice.Simulator(iRollCount);
+
 
         Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
         Console.WriteLine("Total number of rolls = " + rollCount);
         double percentage = 0.0;
 
+        // iterate through the possible die roll totals
         for (int position = 2; position < rollTotals.Length; position++)
         {
+            // calculate what percent of the total rolls each possible die roll was
             percentage = ((float)rollTotals[position] / iRollCount) * 100;
 
+            // create a string of asterisks equal to the number of the percentage, write the possible die rolls to the console with an asterisk for each 1% of the total rolls
             int asterisksCount = (int)Math.Round(percentage);
             Console.Write(position + ": ");
             for (int i = 0; i < asterisksCount; i++)
--- rollDice.cs
+++ rollDice.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mission2Real
 {
+    // define class for rolling the dice
     public class rollDice
     {
         public int[] Simulator(int numRolls)
@@ -13,13 +15,15 @@ namespace Mission2Real
 
             int[] rollTotals = new int[13];
 
+            // initialize variables
             int die1 = 0;
             int die2 = 0;
             int diceTotal = 0;
+            // create an instance of the random object class
             Random random = new Random();
 
-            //put for loop here for int.Parse(rollCount)
-
+            // loop to generate a random number per die as many times as the user selected
+            // add the dice together and count how many times each possible combination showed up
             for (int count = 1; count <= numRolls; count++)
             {
                 die1 = random.Next(1, 7);
