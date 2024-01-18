0b9c5341efd46981db1851c3f2893b9f26a7d227
zekegoodman <zekegoodman@ZEKEGOODMANB9EC>
2024-01-17 18:39:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Mission2Real;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate? (Please enter a number)");
+
+        string rollCount = "";
+        rollCount = Console.ReadLine();
+        int iRollCount = int.Parse(rollCount);
+
+        rollDice rollDice = new rollDice();
+        int[] rollTotals = rollDice.Simulator(iRollCount);
+
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + rollCount);
+        double percentage = 0.0;
+
+        for (int position = 2; position < rollTotals.Length; position++)
+        {
+            percentage = ((float)rollTotals[position] / iRollCount) * 100;
+
+            int asterisksCount = (int)Math.Round(percentage);
+            Console.Write(position + ": ");
+            for (int i = 0; i < asterisksCount; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Thank you for using the dice throwing simulator.   Goodbye!");
+    }
+}
---
+++ rollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Real
+{
+    public class rollDice
+    {
+        public int[] Simulator(int numRolls)
+        {
+
+            int[] rollTotals = new int[13];
+
+            int die1 = 0;
+            int die2 = 0;
+            int diceTotal = 0;
+            Random random = new Random();
+
+            //put for loop here for int.Parse(rollCount)
+
+            for (int count = 1; count <= numRolls; count++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                diceTotal = die1 + die2;
+
+                rollTotals[diceTotal]++;
+            }
+
+            return rollTotals;
+        }
+    }
+}
